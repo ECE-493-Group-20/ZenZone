82833fc12d143544e83896d26cb57be45253b4ba
Tyler Sadler <tsadler@ualberta.ca>
2024-03-05 16:53:21 -0700

    Application may now request microphone permissions from user.

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -18,9 +18,21 @@ function App() {
         >
           Learn React
         </a>
+        <button onClick={getMicrophonePermissions}>Microphone</button>
       </header>
     </div>
   );
+}
+
+// https://developer.mozilla.org/en-US/docs/Web/API/WebRTC_API/Build_a_phone_with_peerjs/Connect_peers/Get_microphone_permission
+function getMicrophonePermissions() {
+  // Chrome saves this permission and handles the querying as well.
+  navigator.mediaDevices
+  .getUserMedia({video:false, audio:true})
+  .catch((err) => {
+    alert("Microphone permissions denied or closed. Please grant microphone permissions to use the microphone.");
+    alert(err);
+  });
 }
 
 export default App;
