5fadb68b5358d19ff4e3dfc04867239dd13c828a
Tyler Sadler <tsadler@ualberta.ca>
2024-03-11 14:52:31 -0600

    Cleaned up microphone code.

--- app/src/microphone.js
+++ app/src/microphone.js
@@ -6,6 +6,8 @@ https://developer.mozilla.org/en-US/docs/Web/API/AnalyserNode/getFloatFrequencyD
 https://www.twilio.com/en-us/blog/audio-visualisation-web-audio-api--react
 https://community.st.com/t5/mems-sensors/is-it-possible-to-measure-sound-intensity-level-from-pcm-samples/td-p/97571
 https://www.ncbi.nlm.nih.gov/books/NBK236684/#:~:text=In%20air%2C%20the%20common%20reference,20%20log%2020%20%3D%2026).
+https://stackoverflow.com/questions/40315433/analysernodes-getfloatfrequencydata-vs-getfloattimedomaindata
+https://decibelpro.app/blog/decibel-chart-of-common-sound-sources/
 */
 
 let state = null;
@@ -17,95 +19,56 @@ let analyserNode;
 let dataArray;
 let bufferLen;
 let updates;
-let avgArray;
+let avg;
 let canvas;
 let canvasCtx;
 
 async function getMicrophonePermissions() {
     if (!window.AudioContext || !window.MediaStreamAudioSourceNode || !window.AudioWorkletNode) {
       alert("Required APIs for sound level measurement not supported by this browser.");
-    } /*else {
-      window.AudioContext = window.AudioContext || window.webkitAudioContext;
-      window.audioContext = new audioContext();
-    }*/
+    }
     // Chrome saves this permission and handles the querying as well.
     const stream = await navigator.mediaDevices
     //navigator.mediaDevices
     .getUserMedia({video:false, audio:true})
-    // .then((stream) => {
-    //   RNSoundLevel.start(MONITOR_INTERVAL);
-    // })
     .catch((err) => {
       alert("Microphone permissions denied or closed. Please grant microphone permissions to use the microphone.");
     });
     let options = {sampleRate:8000};
     audioContext = new AudioContext(options);
-    // await audioContext.audioWorklet.addModule("zenzone-audio-processor.js");
     micStreamAudioSourceNode = audioContext.createMediaStreamSource(stream);
   
     // Create analyser
     analyserNode = audioContext.createAnalyser();
-    analyserNode.fftSize = 32;
+    analyserNode.fftSize = 64;
     bufferLen = analyserNode.frequencyBinCount;
     dataArray = new Float32Array(bufferLen);
-    avgArray = new Float32Array(bufferLen);
+    avg = 0.0;
     updates = 0;
-
-    for (let i = 0; i < bufferLen; i++) {
-      avgArray[i] = 0;
-    }
 
     // Setup network for nodes
     micStreamAudioSourceNode.connect(analyserNode);
     analyserNode.connect(audioContext.destination);
-
-    //audioWorkletNode = new AudioWorkletNode(audioContext, "zenzone-audio-processor");
-    //micStreamAudioSourceNode.connect(audioWorkletNode);
-    console.log("Done")
     state = stream;
-
-    //https://developer.mozilla.org/en-US/docs/Web/API/AnalyserNode/getFloatFrequencyData
-    /*canvas = document.createElement("canvas");
-    canvas.style.position = "absolute";
-    canvas.style.top = "0";
-    canvas.style.left = "0";
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
-    document.body.appendChild(canvas);
-    canvasCtx = canvas.getContext("2d");
-    canvasCtx.clearRect(0, 0, canvas.width, canvas.height);*/
 
     // Start data collection
     measureAudio();
 }
 
-// const getVolume = () => {
-//   if (window.soundMeter && recording) {
-//     let v = window.soundMeter.instant * 200;
-//     setLevel(Math.min(v, 200));
-//     console.log(level);
-//   }
-// }
-
 function stopRecording() {
-  //window.soundMeter.stop();
   micStreamAudioSourceNode.disconnect();
   audioContext.close();
   state?.getTracks().forEach(track => track.stop());
   state = null;
-
   // Send averaged data to server
   uploadAverage();
 }
 
 // TODO: Should be called with some set interval
 function uploadAverage() {
-  let serverArr = new Float32Array(bufferLen);
-  for (let i = 0; i < bufferLen; i++) {
-    serverArr[i] = avgArray[i] / updates;
-  }
+  avg = avg / updates;
   // Make this an actual call to send data to backend
-  console.log("Average!", serverArr);
+  console.log("Average!", avg);
 }
 
 export function toggleMicrophone() {
@@ -117,6 +80,7 @@ export function toggleMicrophone() {
 }
 
 // https://developer.mozilla.org/en-US/docs/Web/API/AnalyserNode/getFloatFrequencyData
+// https://stackoverflow.com/questions/40315433/analysernodes-getfloatfrequencydata-vs-getfloattimedomaindata
 function measureAudio() {
   // Schedule next measure period if we want to keep recoding. Apparently scheduled by the browser.
   if (state) {
@@ -138,28 +102,8 @@ function measureAudio() {
       // 10dB used as a "clip" value as this is the about the sound level of breathing.
       // https://decibelpro.app/blog/decibel-chart-of-common-sound-sources/
       let db = Math.max(10, 20 * Math.log10(sum / (20 * Math.pow(10, -6))));
-      avgArray[0] += db;
+      avg += db;
       console.log("DB level:", db);
     }
-
-    //https://developer.mozilla.org/en-US/docs/Web/API/AnalyserNode/getFloatFrequencyData
-    //Draw black background
-    /*canvasCtx.fillStyle = "rgb(0 0 0)";
-    canvasCtx.fillRect(0, 0, canvas.width, canvas.height);
-
-    //Draw spectrum
-    const barWidth = (canvas.width / bufferLen);
-    let posX = 0;
-    for (let i = 0; i < bufferLen; i++) {
-      const barHeight = (dataArray[i] + 140) * 10;
-      canvasCtx.fillStyle = `rgb(${Math.floor(barHeight + 100)} 50 50)`;
-      canvasCtx.fillRect(
-        posX,
-        canvas.height - barHeight / 2,
-        barWidth,
-        barHeight / 2,
-      );
-      posX += barWidth + 1;
-    }*/
   }
 }
