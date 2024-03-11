0741d5dc2f8902b3d5ea91df0ed59f98a536c45b
justin javier <javierjustin7@gmail.com>
2024-03-11 00:11:42 -0600

    feat: markers

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,9 +1,11 @@
 import './App.css';
-import Map from './map/Map';
+import Map from './components/map/Map';
+import Logo from './logo.png'
 
 function App() {
   return (
     <div className="App">
+      <img className='logo' src={Logo}/>
       <Map />
     </div>
   );
---
+++ app/src/components/map/Map.tsx
@@ -0,0 +1,56 @@
+import { useCallback, useState } from "react"
+import {GoogleMap, GoogleMapProps, useJsApiLoader} from "@react-google-maps/api"
+import "./index.css"
+import { LinearProgress } from "@mui/material";
+import CustomMarker from "../marker/Marker"
+
+
+const Map = (props: GoogleMapProps) => {
+    const { isLoaded } = useJsApiLoader({
+        id: 'test-script',
+        googleMapsApiKey: process.env.REACT_APP_MAP_API_KEY || '', // !!! PROD KEY IS RESTRICTED TO WEBSITE
+    });
+    
+    const center = {
+        lat: 53.53,
+        lng: -113.52,
+    };
+
+    const [map, setMap] = useState<google.maps.Map | null>();
+
+    const onLoad = useCallback((map: google.maps.Map) => {
+        const bounds = new window.google.maps.LatLngBounds(center);
+        map.fitBounds(bounds);
+        // fetching data to create markers
+        setMap(map);
+    }, []);
+
+    const onUnmount = useCallback(() => {
+        setMap(null);
+    }, []);
+
+    return isLoaded ? (
+        <GoogleMap
+            center={center}
+            mapContainerClassName="map"        
+            onLoad={onLoad}
+            onUnmount={onUnmount}   
+            zoom={10}
+            options={{
+                disableDefaultUI: true,                
+                styles: require("./mapStyle.json"),
+            }}
+            {...props}
+        >
+            <CustomMarker position={{lat: 53.53, lng: -113.52,}}/>
+            <CustomMarker position={{lat: 53, lng: -113,}}/>
+        </GoogleMap>
+        )
+        :
+        <div className="map">
+            <LinearProgress />
+        </div>
+}
+
+export default Map;
+
