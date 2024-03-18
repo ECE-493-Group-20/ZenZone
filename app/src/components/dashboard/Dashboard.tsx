9602fcaec37ac485507b68f93a424e26c09b1af2
justin javier <javierjustin7@gmail.com>
2024-03-17 22:31:01 -0600

    feat: dashboard

--- app/src/components/dashboard/Dashboard.tsx
+++ app/src/components/dashboard/Dashboard.tsx
@@ -1,12 +1,55 @@
-import { Drawer } from "@mui/material"
+import { Drawer, LinearProgress } from "@mui/material"
 import { useDashboard } from "./dashboardprovider/DashboardProvider"
+import { LinePlot } from "@mui/x-charts/LineChart"
+import "./index.css"
+import { ResponsiveChartContainer } from "@mui/x-charts/ResponsiveChartContainer"
+import { ChartsXAxis } from '@mui/x-charts/ChartsXAxis'
 
 export const Dashboard = () => {
     const {open, setOpen} = useDashboard()
 
     return (
-        <Drawer variant="temporary" anchor='bottom' open={open} onClose={() => setOpen(false)}>
-          <div>TEST</div>
+        <Drawer 
+          className="drawer" 
+          variant="temporary" 
+          anchor='bottom' 
+          open={open} 
+          onClose={() => setOpen(false)}>
+          <div className="paper">
+            <h1>{"{LOCATION NAME}"}</h1>
+            <p>{"{LOCATION}"}</p>
+            <div className="levelCardContainer">
+              <div className="levelCard">
+                <h4>Busy Level</h4>
+                <LinearProgress title="Busy Level" variant="determinate" value={50} sx={{'& .MuiLinearProgress-bar': {backgroundColor: 'red'}}}/>
+              </div>
+              <div className="levelCard">
+                <h4>Sound Level</h4>
+                <LinearProgress title="Sound Level" variant="determinate" value={80}/>
+              </div>
+            </div>
+            
+            <div style={{width: '100vw', height: '40vh'}}>
+            <ResponsiveChartContainer 
+              xAxis={[
+                {
+                  data: ['A', 'B', 'C', 'D', 'E'],
+                  scaleType: 'band',
+                  id: 'x-axis-id',
+                },
+              ]}
+              series={[{
+                  type: 'line',
+                  data: [1,2,3,4,5]
+                }, {
+                  type: 'line',
+                  data: [5,4,3,2,1]
+                }]}>
+                  <LinePlot/>
+                  <ChartsXAxis label="X axis" position="bottom" axisId="x-axis-id" />
+            </ResponsiveChartContainer>
+            </div>  
+          </div>
         </Drawer>
     )
 }
