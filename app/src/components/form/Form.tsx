04453325084e7640424b8ecb0b63d8d53e7aae8f
Raquib <r.raquib01@gmail.com>
2024-04-03 04:07:48 -0600

    add chips for input form

--- app/src/components/form/Form.tsx
+++ app/src/components/form/Form.tsx
@@ -1,4 +1,4 @@
-import { Button, Slider, SxProps } from '@mui/material'
+import { Button, Chip, Slider, SxProps } from '@mui/material'
 import './index.css'
 import { useState } from 'react'
 import { Check, Close } from '@mui/icons-material'
@@ -31,6 +31,46 @@ const Form = (props: FormProps) => {
         }
         return [colors[colors.length - 1], chosenLabels[chosenLabels.length - 1]] // default return last value in colors
     }
+
+    const valueToColorDescriptor = (value: number, type: LevelType) => {
+      const soundLevels = [20, 40, 60, 80, 120]; // in dB
+      const busyLevels = [20, 40, 60, 80, 95]; // in % (subject to change)
+      const colors = ["#7765AC", "#4D74B2", "#659360", "#FCB955", "#E95658"];
+      const soundLabels = ["FAINT", "SOFT", "MODERATE", "LOUD", "VERY LOUD"];
+      const busyLabels = [
+        "NOT BUSY",
+        "NOT VERY BUSY",
+        "NORMAL",
+        "BUSY",
+        "VERY BUSY",
+      ];
+      const chosenLevels = type === "busy" ? busyLevels : soundLevels;
+      const chosenLabels = type === "busy" ? busyLabels : soundLabels;
+      for (let i = 0; i < colors.length; i++) {
+        if (value <= chosenLevels[i]) {
+          return (
+            <Chip
+              label={chosenLabels[i]}
+              variant="filled"
+              sx={{
+                backgroundColor: colors[i],
+                marginLeft: 1,
+              }}
+            />
+          );
+        }
+      }
+      return (
+        <Chip
+          label={chosenLabels[chosenLabels.length - 1]}
+          variant="filled"
+          sx={{
+            backgroundColor: colors[colors.length - 1],
+            marginLeft: 1,
+          }}
+        />
+      );
+    };
 
     const soundColor = valueToColor(soundLevel, 'sound')
     const busyColor = valueToColor(busyLevel, 'busy')
@@ -89,18 +129,21 @@ const Form = (props: FormProps) => {
         }
     }
 
+    const soundChip = valueToColorDescriptor(soundLevel, "sound");
+    const busyChip = valueToColorDescriptor(busyLevel, "busy");
+
     return (
         <div className="form">
             <h1>Manual Input</h1>
 
             <div className="levelCardTitle">
-                <h4>Sound Level | {soundColor[1]}</h4>
+                <h4>Sound Level {soundChip}</h4>
                 <p>{soundLevel}dB</p>
             </div>
             <Slider value={soundLevel} onChange={(_, value) => setSoundLevel(value as number)} sx={sliderStyle['sound']}/>
             
             <div className="levelCardTitle">
-                <h4>Busy Level | {busyColor[1]}</h4>
+                <h4>Busy Level {busyChip}</h4>
                 <p>{busyLevel}%</p>
             </div>
             <Slider value={busyLevel} onChange={(_, value) => setBusyLevel(value as number)} sx={sliderStyle['busy']}/>
