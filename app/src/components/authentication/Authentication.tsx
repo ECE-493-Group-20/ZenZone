095f3858326d38f37e1af8d4667c09c489aee188
Raquib Khan Lavani <66289290+raquibk@users.noreply.github.com>
2024-04-03 14:09:28 -0600

    Revert "User Interface Design Improvements"

--- app/src/components/authentication/Authentication.tsx
+++ app/src/components/authentication/Authentication.tsx
@@ -19,7 +19,6 @@ import Container from '@mui/material/Container';
 import { Link as RouteLink } from "react-router-dom";
 import "../../App.css";
 import {useNavigate, redirectDocument} from "react-router-dom";
-import Logo from "../logo/Logo";
 
 
 function UserSignIn() {
@@ -67,73 +66,81 @@ function UserSignIn() {
   };
 
   return (
-    <div style={{ height: "100vh" }}>
-      <Button component={RouteLink} to={"/"}>
-        Home
-      </Button>
+    <>
+    <Button component={RouteLink} to={"/"}>Home</Button>
       <Container component="main" maxWidth="xs">
         <CssBaseline />
         <Box
           sx={{
             marginTop: 8,
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
           }}
         >
-          <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
             <LockOutlinedIcon />
           </Avatar>
-          <Link href="/">
-            <Logo className="logo" style={{ margin: 1 }} />
-          </Link>
           <Typography component="h1" variant="h5">
             Sign in {user?.email}
           </Typography>
+          
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
+              autoFocus
+              inputRef={emailRef}
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+              inputRef={passwordRef}
+            />
 
-          <TextField
-            margin="normal"
-            required
-            fullWidth
-            id="email"
-            label="Email Address"
-            name="email"
-            autoComplete="email"
-            autoFocus
-            inputRef={emailRef}
-          />
-          <TextField
-            margin="normal"
-            required
-            fullWidth
-            name="password"
-            label="Password"
-            type="password"
-            id="password"
-            autoComplete="current-password"
-            inputRef={passwordRef}
-          />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              onClick={signIn}
+            >
+              Sign In
+            </Button>
 
-          <Button type="submit" fullWidth variant="contained" onClick={signIn}>
-            Sign In
-          </Button>
+            <Typography component="h1" variant="h5" align='center'>
+            Or
+            </Typography>
 
-          <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            sx={{ mt: 3, mb: 2 }}
-            onClick={googleSignIn}
-          >
-            Continue with Google
-          </Button>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+              onClick={googleSignIn}
+            >
+              Continue with Google
+            </Button>
 
-          <Link component={RouteLink} to="/register">
-            {"Don't have an account? Sign Up"}
-          </Link>
-        </Box>
+            <Grid container>
+              <Grid item>
+                <Link component={RouteLink} to="/register">
+                  {"Don't have an account? Sign Up"}
+                </Link>
+              </Grid>
+            </Grid>
+          </Box>
       </Container>
-    </div>
+    </>
   );
 }
 
